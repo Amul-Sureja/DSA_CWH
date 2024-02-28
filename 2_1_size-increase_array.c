72564e1ec0276bd4c67aa7be3e986ea77a3051e9
Amul <amulsurejapi@gmail.com>
2024-02-28 10:54:58 +0530

    DSA

--- 2_1_size-increase_array.c
+++ 2_1_size-increase_array.c
@@ -1,55 +1,55 @@
-// //************************************************using main function
-// #include <stdio.h>
-// #include <stdlib.h>
+//************************************************using main function
+#include <stdio.h>
+#include <stdlib.h>
 
-// int main() {
-//     int initialCapacity = 5;
-//     int *array = (int *)malloc(initialCapacity * sizeof(int));
-//     if (array == NULL) {
-//         printf("Memory allocation failed.\n");
-//         return 1;
-//     }
+int main() {
+    int initialCapacity = 5;
+    int *array = (int *)malloc(initialCapacity * sizeof(int));
+    if (array == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
 
-//     int size = 0; // Current size of the array
-//     int i;
+    int size = 0; // Current size of the array
+    int i;
 
-//     // Add some elements to the array
-//     for (i = 0; i < initialCapacity; i++) {
-//         array[size++] = i;
-//     }
+    // Add some elements to the array
+    for (i = 0; i < initialCapacity; i++) {
+        array[size++] = i;
+    }
 
-//     // Print the original array
-//     printf("Original array:\n");
-//     for (i = 0; i < size; i++) {
-//         printf("%d ", array[i]);
-//     }
-//     printf("\n");
+    // Print the original array
+    printf("Original array:\n");
+    for (i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 
-//     // Increase the size of the array
-//     int newSize = initialCapacity * 2; // New size of the array
-//     array = (int *)realloc(array, newSize * sizeof(int));
-//     if (array == NULL) {
-//         printf("Memory reallocation failed.\n");
-//         return 1;
-//     }
+    // Increase the size of the array
+    int newSize = initialCapacity * 2; // New size of the array
+    array = (int *)realloc(array, newSize * sizeof(int));
+    if (array == NULL) {
+        printf("Memory reallocation failed.\n");
+        return 1;
+    }
 
-//     // Add more elements to the resized array
-//     for (i = initialCapacity; i < newSize; i++) {
-//         array[size++] = i;
-//     }
+    // Add more elements to the resized array
+    for (i = initialCapacity; i < newSize; i++) {
+        array[size++] = i;
+    }
 
-//     // Print the resized array
-//     printf("Resized array:\n");
-//     for (i = 0; i < size; i++) {
-//         printf("%d ", array[i]);
-//     }
-//     printf("\n");
+    // Print the resized array
+    printf("Resized array:\n");
+    for (i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 
-//     // Free the dynamically allocated memory
-//     free(array);
+    // Free the dynamically allocated memory
+    free(array);
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 // //*************************************************using function
