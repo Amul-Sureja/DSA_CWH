14a100a7ca9fec382158d9db0ed6c3f6c9685eb0
Amul <amulsurejapi@gmail.com>
2024-02-28 11:02:42 +0530

    DSA

--- 4_deletation-of-array.c
+++ 4_deletation-of-array.c
@@ -10,9 +10,14 @@ void display(int arr[], int n)
     printf("\n");
 }
  
-void indDeletion(int arr[], int size, int index)
+void indDeletion(int arr[], int size, int index, int capacity)
 {
     // code for Deletion
+    if (index >= capacity-1)
+    {
+        printf("Sorry not deletion");
+    }
+    
     for (int i = index; i < size-1; i++)
     {
         arr[i] = arr[i + 1];
@@ -22,9 +27,9 @@ void indDeletion(int arr[], int size, int index)
 int main()
 {
     int arr[100] = {7, 8, 12, 27, 88};
-    int size = 5, element = 45, index = 3;
+    int size = 5, element = 45, index = 3, capacity = 100;
     display(arr, size);
-    indDeletion(arr, size, index);
+    indDeletion(arr, size, index, capacity);
     size -= 1;
     display(arr, size);
     return 0;
