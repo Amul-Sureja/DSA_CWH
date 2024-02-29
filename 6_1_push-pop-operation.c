248cdbe333adfd3ea5ab2b6319d8fda5daf3f28d
Amul <amulsurejapi@gmail.com>
2024-02-29 08:42:03 +0530

    DSA

---
+++ 6_1_push-pop-operation.c
@@ -0,0 +1,15 @@
+// 1] push(): When we insert an element in a stack then the operation is known as a push. If the stack is full then the overflow condition occurs.
+// 2] pop(): When we delete an element from the stack, the operation is known as a pop. If the stack is empty means that no element exists in the stack, this state is known as an underflow state.
+// 3] isEmpty(): It determines whether the stack is empty or not.
+// 4] isFull(): It determines whether the stack is full or not.'
+// 5] peek(): It returns the element at the given position.
+// 6] count(): It returns the total number of elements available in a stack.
+// 7] change(): It changes the element at the given position.
+// 8] display(): It prints all the elements available in the stack.
+
+
+// Infix to prefix
+// Infix to postfix
+// Prefix to infix
+// Prefix to postfix
+// Postfix to infix
