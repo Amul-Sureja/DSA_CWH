64c3c498aa179dffbad309cdd31953d639507710
Amul <amulsurejapi@gmail.com>
2024-02-29 10:30:18 +0530

    DSA

--- 13_2_push-pop-isempty-isfull-peek.c
+++ 13_2_push-pop-isempty-isfull-peek.c
@@ -45,7 +45,7 @@ void push(struct stack *ptr, int val)
     }
 }
 
-int pop(struct stack *ptr) 
+int pop(struct stack *ptr)
 {
     if (isEmpty(ptr))
     {
@@ -60,11 +60,25 @@ int pop(struct stack *ptr)
     }
 }
 
+int peek(struct stack *sp, int i)
+{
+    int arrayInd = sp->top - i + 1;
+    if (arrayInd < 0)
+    {
+        printf("Not a valid position for the stack\n");
+        return -1;
+    }
+    else
+    {
+        return sp->arr[arrayInd];
+    }
+}
+
 int main()
 {
     struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
     sp->size = 10;
-    sp->top = -1; 
+    sp->top = -1;
     sp->arr = (int *)malloc(sp->size * sizeof(int));
     printf("Stack has been created successfully\n");
 
@@ -73,22 +87,30 @@ int main()
 
     push(sp, 1);
     push(sp, 23);
-    push(sp, 99);
+    push(sp, 9);
     push(sp, 75);
-    push(sp, 3);
+    push(sp, 85);
     push(sp, 64);
     push(sp, 57);
-    push(sp, 46);
+    push(sp, 88);
     push(sp, 89);
-    push(sp, 6); // ---> Push end 10 values
-    push(sp, 46); // Stack Overflow since the size of the stack is 10
+    push(sp, 852); // ---> Push end 10 values
+
+    // Printing values from the stack
+    for (int j = 1; j <= sp->top + 1; j++)
+    {
+        printf("The value at position %d is %d\n", j, peek(sp, j));
+    }
+
+    push(sp, 46);  // Stack Overflow since the size of the stack is 10
     push(sp, 100); // Stack Overflow since the size of the stack is 10
- 
+
     printf("After pushing, Full: %d\n", isFull(sp));
     printf("After pushing, Empty: %d\n", isEmpty(sp));
 
     printf("Popped %d from the stack\n", pop(sp)); // --> Last in first out!
     printf("Popped %d from the stack\n", pop(sp)); // --> Last in first out!
     printf("Popped %d from the stack\n", pop(sp)); // --> Last in first out!
+
     return 0;
 }
