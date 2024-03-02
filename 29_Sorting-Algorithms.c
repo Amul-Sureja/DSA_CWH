3d50fa2da5d5bb2831b4107d8b2fb6784d8e2fa5
Amul <amulsurejapi@gmail.com>
2024-03-02 13:08:23 +0530

    DSA

---
+++ 29_Sorting-Algorithms.c
@@ -0,0 +1,42 @@
+// two type of shorting algorithms
+// 1] Sorting in ascending order.
+// 2] Sorting in descending order.
+
+// type of sorting algorithma
+// 1] Bubble sort
+// 2] Bucket sort
+// 3] Comb sort
+// 4] Counting sort
+// 5] Heap sort
+// 6] Insertion sort
+// 7] Merge sort
+// 8] Quick sort
+// 9] Radix sort
+// 10] Selection sort
+// 11] Shell sort
+// 12] Bitonic sort
+// 13] Cocktail sort
+// 14] Cycle sort
+// 15] Tim sort
+// 16] Bingo Sort Algorithm
+// 17] Pigeonhole Sort
+// 18] Strand Sort
+// 19] Pancake sorting
+// 20] BogoSort or Permutation Sort
+// 21] Gnome Sort
+// 22] Sleep Sort – The King of Laziness
+// 23] Structure Sorting in C++
+// 24] Stooge Sort
+// 25] Tag Sort (To get both sorted and original)
+// 26] Tree Sort
+// 27] Odd-Even Sort / Brick Sort
+// 28] 3-way Merge Sort
+
+
+// Library Implementations:
+// 1] Introsort – C++’s Sorting Weapon
+// 2] Comparator function of qsort() in C
+// 3] sort() in C++ STL
+// 4] C qsort() vs C++ sort()
+// 5] Arrays.sort() in Java with examples
+// 6] Collections.sort() in Java with Examples
