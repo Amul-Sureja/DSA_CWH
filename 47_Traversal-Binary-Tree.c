3ec48034a15cc722fdb442a1e34278e6ad69984d
Amul <amulsurejapi@gmail.com>
2024-03-05 01:29:27 +0530

    DSA

---
+++ 47_Traversal-Binary-Tree.c
@@ -0,0 +1,17 @@
+// void preOrder(struct  node* root){
+//     if(root!=NULL){
+//         printf("%d ", root->data);
+//         preOrder(root->left);
+//         preOrder(root->right);
+//     }
+// }
+
+// Traversal in Binary Tree
+
+// 1] InOrder
+            // left subtree ==> root ==> right subtree
+// 2] PostOrder 
+            //  root ==> left subtree ==> right subtree
+// 3] PreOrder 
+            // left subtree ==> right subtree ==> root
+
