315edc03ac91c8a2304a38c031bdd8e1ee41c803
Amul <amulsurejapi@gmail.com>
2024-03-06 21:36:58 +0530

    DSA

--- 60_Graphs.c
+++ 60_Graphs.c
@@ -1,3 +1,15 @@
 // Graph representation
 // 1] Sequential representation (or, Adjacency matrix representation)
 // 2] Linked list representation (or, Adjacency list representation)
+
+// There are two primary ways to traverse a graph, which are:
+// Breadth-First Search (BFS)
+// Depth-First Search (DFS)
+
+
+// Vertex:	A point or an individual node in a graph.
+// Edge:	A connection between two vertices.
+// Root:	The vertex from which the traversal starts.
+// BFS:	Breadth-First Search is a method for exploring the vertex level by level.
+// DFS:	Depth-First Search is a method for exploring the vertex deeply before moving to the next level.
+
