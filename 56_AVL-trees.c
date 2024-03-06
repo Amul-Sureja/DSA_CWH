fed268d612a5eef1f6f81abda1c5259df7898ab8
Amul <amulsurejapi@gmail.com>
2024-03-06 19:42:33 +0530

    DSA

---
+++ 56_AVL-trees.c
@@ -0,0 +1,6 @@
+// => height balanced binary search trees.
+// => height diffrence between of left and right sebtrees for every node is less than or equal to 1.
+// => balanced factor = height of right subtree - height of left subtree
+// |B.F.| <= 1
+// => can be -1, 0 or 1 for a node to be balanced in a binary tree.
+// => can be -1, 0 or 1 for all nodes of an AVL tree.
